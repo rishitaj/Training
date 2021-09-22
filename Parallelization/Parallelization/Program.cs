b2124fe956b98401402f9e7f66a4f9de8b25c9e5
rishitaj <rishitajain8011@gmail.com>
2021-09-22 12:30:42 +0530

    Handson on parallelization

---
+++ Parallelization/Parallelization/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Parallelization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the num: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //Serial Programming
+            for(int i=0;i<num;i++)
+            {
+                if(i%2==0)
+                    Console.WriteLine($"Even number: {i}, thread={Thread.CurrentThread.ManagedThreadId}");
+                else
+                    Console.WriteLine($"Odd number: {i}, thread={Thread.CurrentThread.ManagedThreadId}");
+
+            }
+
+            Console.WriteLine("********************");
+            //Parallel programming
+            Parallel.For(0, num, i =>
+             {
+                  if (i % 2 == 0)
+                      Console.WriteLine($"Even number: {i}, thread={Thread.CurrentThread.ManagedThreadId}");
+                  else
+                      Console.WriteLine($"Odd number: {i}, thread={Thread.CurrentThread.ManagedThreadId}");
+             });
+            Console.Read();
+        }
+    }
+}
